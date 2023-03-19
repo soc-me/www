1d2188a338b9a7d9333852955d5604f06c81bb55
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-19 18:27:53 +0000

    git test

--- pages/index.js
+++ pages/index.js
@@ -6,32 +6,8 @@ import axios from '@/lib/axios'
 export default function Home() {
   const {user,  login } = useAuth({
       middleware: 'guest',
-      //redirectIfAuthenticated: '/authtest',
   })
-  const name = 'thenu'
-  const email  = 'thenu@thenu.com'
-  const password = '12345678'
-  const [errors, setErrors] = useState('No errors')
-  const [status, setStatus] = useState('No status')
-  const registerUser = () => {
-    login({
-      email,
-      password,
-      remember: false,
-      setErrors,
-      setStatus,
-  })
-  }
 
-  if(user){
-    axios
-    .get('/api/posts')
-    .then(response => console.log(response.data))
-    .catch(error => {
-        if (error.response.status !== 422) throw error
-        console.log(error)
-    })
-  }
   
   return (
     <>
