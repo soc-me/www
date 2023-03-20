63adabda39314308bdf214ab6fd910042f1b87c2
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-20 14:39:08 +0000

    created buttons for home page

--- components/Pages/Index/IndexPage.js
+++ components/Pages/Index/IndexPage.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { IndexContainer } from "./IndexPage.styled";
 
 const IndexPage = () => {
+    const [onlyFollowing, setOnlyFollowing] = useState(false);
     return (
-        <IndexContainer>
-            
+        <IndexContainer className="center">
+            <div className="indexInner">
+                {/*To do: Add side bar  */}
+                <div className="postsOuter">
+                    <div className="typeSelect">
+                        <button className={`all selected-${!onlyFollowing}`} onClick={() => setOnlyFollowing(false)} >
+                            <span>All Posts</span>
+                        </button>
+                        <button className={`true selected-${onlyFollowing}`} onClick={() => setOnlyFollowing(true)}>
+                            <span>Following</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </IndexContainer>
     );
 }
--- components/Pages/Index/IndexPage.styled.js
+++ components/Pages/Index/IndexPage.styled.js
@@ -1,7 +1,38 @@
+import { GLOBAL } from "@/GLOBAL";
 import styled from "styled-components";
 
 export const IndexContainer = styled.div`
     width: 100%;
-    display: flex; flex-direction: column;
     margin-top: 30px;
+    & .indexInner{
+        width: 100%;
+        display: flex; align-items: flex-start;
+        //remove
+        justify-content: center;
+        & .postsOuter{
+            display: flex; flex-direction: column;
+            width: clamp(300px, 55%, 1000px);
+            & .typeSelect{
+                display: flex; align-items: center;
+                & button{
+                    background-color: transparent;
+                    cursor: pointer;
+                    display: flex; align-items: center; justify-content: center;
+                    &:first-of-type{margin-right: 20px;}
+                    border-radius: 5px;
+                    & span{
+                        padding-bottom: 5px;
+                        font-weight: 600;
+                        font-size: 1em;
+                        border-bottom: 3px solid transparent;
+                    }
+                    &.selected-true{
+                        & span{
+                            border-bottom: 3px solid ${GLOBAL.LOADER_COLOR};
+                        }
+                    }
+                }
+            }
+        }
+    }
 `
