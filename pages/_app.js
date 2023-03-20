ef13afccbbeba94f0a48ab91af980adc88acbbc5
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-20 11:31:33 +0000

    styled-sheets and nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
+import '../components/global.css'
+import NextNProgress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <NextNProgress color="blue" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
+    <Component {...pageProps} />
+  </>
+  )
 }
