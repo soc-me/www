94ac1d5245f2578d84b3a7972dfb6b3db77cbac8
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-05-12 14:23:44 +0100

    added google analyticsq

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '../components/global.css'
 import NextNProgress from 'nextjs-progressbar'
 import { GLOBAL } from '@/GLOBAL'
-// light blue color: #2e9cca
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '@/lib/gtag'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
   return (
   <>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-G227SJ7Q8W" strategy="afterInteractive" />
+    <Script id='google-analytics' strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag("config", "G-G227SJ7Q8W");
+        `}
+    </Script>
     <NextNProgress color={GLOBAL.LOADER_COLOR} startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={false} />
     <Component {...pageProps} />
   </>
